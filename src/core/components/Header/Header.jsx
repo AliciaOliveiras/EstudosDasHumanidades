541059701f8cf90d6d9f2db8b6691314853279a2
Alícia de Oliveira <aliciaolivsantos@uol.com.br>
2023-07-05 16:53:27 -0300

    trocando de appbar para div

--- src/core/components/Header/Header.jsx
+++ src/core/components/Header/Header.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import styles from './Header.module.css'
 
 function Header() {
 
   return (
 <>
-<AppBar style={{backgroundColor: '#078272', display: 'flex', justifyContent:'center', alignItems: "center", padding: '1rem'}} position="static">
+<div style={{backgroundColor: '#078272', display: 'flex', justifyContent:'center', alignItems: "center", padding: '1rem', color: 'white'}} position="static">
       <Toolbar style={{display: 'grid'}} >
       <Typography style={{fontFamily: 'Poppins, sans-serif', fontSize: '1.5rem'}}>
           HACA03 - Estudos das Humanidades
         </Typography>
       </Toolbar>
-    </AppBar>
+    </div>
 </>
   );
 }
